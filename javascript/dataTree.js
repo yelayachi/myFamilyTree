e4efa0ea18a28453b8f0ae6af0216426eefe73b7
Anouar Hamed <anouar@MacBook-Air-de-Anouar.local>
2013-01-25 23:14:07 +0100

    First commit

---
+++ javascript/dataTree.js
@@ -0,0 +1,29 @@
+var treeJson = {
+        id: "node01",
+        name: "Anouar Hamed Hamed Hamed Hamed 01",
+		birthDate:"01/01/1986",
+		photoUrl:"images/people.png",
+		partners:[],
+        children: [{
+            id: "node02",
+            name: "Anouar 02",
+			birthDate:"01/01/1986",
+			photoUrl:"images/people.png",
+			partners:[],
+            children: []
+        },
+		{	id: "node03",
+            name: "Anouar 03",
+			birthDate:"01/01/1986",
+			photoUrl:"images/people.png",
+			partners:[],
+            children: []
+        },
+		{	id: "node04",
+            name: "Anouar 04",
+			birthDate:"01/01/1986",
+			photoUrl:"images/people.png",
+			partners:[],
+            children: []
+        }]
+    };
---
+++ javascript/myFamilyTree.js
@@ -0,0 +1,72 @@
+﻿
+	
+	  
+$(document).ready(function() {
+	
+	// Load the data
+	var tree = Arboreal.parse(treeJson, 'children');
+	
+	var stage = new Kinetic.Stage({
+        container: 'treeCanvas',
+        width: 1500,
+        height: 800
+    });
+	  
+	var layer = new Kinetic.Layer();
+	
+	tree.traverseDown(function (node) {
+		var identityGroup = new Kinetic.Group({
+			draggable: true
+		});
+		var card = new Kinetic.Rect({
+			width: 400,
+			height: 150,
+			fill: 'green',
+			strokeWidth: 3,
+			name: node.id
+		});
+		var headShot = new Kinetic.Image({
+			x: 5,
+			y: 5,
+			width: 128,
+			height: 128
+		});
+		var imageObj = new Image();
+		imageObj.onload = function() {
+			headShot.setImage(imageObj);
+		};
+		imageObj.src= node.data.photoUrl;
+		var fullName = new Kinetic.Text({
+			x: 132,
+			y: 5,
+			text: node.data.name,
+			fontSize: 24,
+			fontFamily: 'Calibri',
+			fill: 'blue',
+			width: 250,
+			align: 'center'
+		});
+		var nodeInfo = new Kinetic.Text({
+			x: 132,
+			y: 100,
+			text: "Date de naissance : "+node.data.birthDate,
+			fontSize: 14,
+			fontFamily: 'Calibri',
+			fill: 'blue',
+			width: 250
+		});
+		
+		identityGroup.add(card);
+		identityGroup.add(headShot);
+		identityGroup.add(fullName);
+		identityGroup.add(nodeInfo);
+		layer.add(identityGroup);
+	});
+	
+	stage.add(layer);
+   
+   
+   
+ });
+
+
