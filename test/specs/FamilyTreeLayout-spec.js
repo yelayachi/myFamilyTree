4fc3d9ba94fee62737ec65604b86ab8e89dd2036
Anouar Hamed <anouar@hamed.com>
2013-02-13 17:31:39 +0100

    Création du TreeLayout

---
+++ test/specs/FamilyTreeLayout-spec.js
@@ -0,0 +1,36 @@
+function populatedTree() {
+	var tree = new Arboreal();
+	tree
+	.appendChild()
+	.appendChild()
+	.appendChild();
+	
+	tree.children[0]
+	.appendChild()
+	.appendChild();
+	
+	tree.children[2]
+	.appendChild()
+	.appendChild();
+	
+	tree.children[0].children[0]
+	.appendChild()
+	.appendChild()
+	.appendChild()
+	.appendChild()
+	.appendChild();
+	
+	tree.children[2].children[0]
+	.appendChild()
+	.appendChild();
+	
+	return tree;
+}
+
+describe("Family Tree", function () {
+	it("#positionTree()", function () {
+		var tree = populatedTree();
+		var layout = new FamilyTreeLayout();
+		layout.positionTree(tree);
+	});
+});
